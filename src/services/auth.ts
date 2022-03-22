dbd019bc8d3b7c1ed087feacf9a146cc6eaf8dc9
4shutosh <4shutoshsingh@gmail.com>
2022-03-22 11:42:52 +0530

    firebase login and api implemented

--- src/services/auth.ts
+++ src/services/auth.ts
@@ -1,5 +1,21 @@
 import {removeWindowClass} from '@app/utils/helpers';
 import {Gatekeeper} from 'gatekeeper-client-sdk';
+import dotenv from 'dotenv';
+import {initializeApp} from 'firebase/app';
+import * as firebaseAuth from 'firebase/auth';
+import axios from 'axios';
+import {User} from 'firebase/auth';
+
+dotenv.config();
+initializeApp({
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_DATABASE_URL,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID
+});
 
 export const loginByAuth = async (email: string, password: string) => {
   const token = await Gatekeeper.loginByAuth(email, password);
@@ -24,6 +40,38 @@ export const loginByGoogle = async () => {
   removeWindowClass('hold-transition');
   return token;
 };
+
+const firebaseGoogleProvider = new firebaseAuth.GoogleAuthProvider();
+export const loginByFirebaseAndAPI = async () => {
+  const token = await firebaseAuth
+    .signInWithPopup(firebaseAuth.getAuth(), firebaseGoogleProvider)
+    .then((result) => {
+      const loggedInUser = result.user;
+      console.log('GLogin Success, hitting API now!');
+      return loginCollegeAPI(loggedInUser);
+    })
+    .catch((error) => {
+      console.log(error.message);
+      alert(error.message);
+    });
+  return token;
+};
+
+async function loginCollegeAPI(loggedInUser: User) {
+  const token = await axios
+    .post(
+      `${process.env.REACT_APP_BASE_API_URL}/login?email=${loggedInUser.email}&name=${loggedInUser.displayName}&imageUrl=${loggedInUser.photoURL}`
+    )
+    .then((response) => response.data)
+    .then((data) => {
+      localStorage.setItem('token', data.accessToken);
+      return data.accessToken;
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+  return token;
+}
 
 export const registerByGoogle = async () => {
   const token = await Gatekeeper.registerByGoogle();
