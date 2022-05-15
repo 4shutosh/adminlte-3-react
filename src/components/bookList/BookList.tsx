a5d09029cc97337b7f171e7b267afdac08be10bb
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2022-05-15 20:08:55 +0530

    barbones QR logic and components ready

--- src/components/bookList/BookList.tsx
+++ src/components/bookList/BookList.tsx
@@ -1,15 +1,26 @@
 import React, {useEffect} from 'react';
 import {baseUrl} from '@app/globalVars';
-import {GridColDef, DataGrid} from '@mui/x-data-grid';
+import {GridColDef, DataGrid, GridSelectionModel} from '@mui/x-data-grid';
 import {useSelector} from 'react-redux';
 import axios from 'axios';
 
-const BookList = () => {
+const BookList = ({getSelection}: any) => {
+  const [selection, setSelection] = React.useState<GridSelectionModel>([]);
   const [rows, setRows] = React.useState([]);
   const [loading, setLoading] = React.useState(true);
   const [columns, setColumns] = React.useState<GridColDef[]>([]);
   const token: string = useSelector((state: any) => state.auth.token);
   const getBooksApi = `${baseUrl}/books`;
+
+  const handleSelection = (newSelection: any) => {
+    setSelection(newSelection);
+    const selectedIds = new Set(newSelection);
+    const selectedRows = rows.filter((row: any) => {
+      return selectedIds.has(row.bookId);
+    });
+    // console.log(selectedRows);
+    getSelection(selectedRows);
+  };
 
   useEffect(() => {
     axios
@@ -49,11 +60,14 @@ const BookList = () => {
   return (
     <DataGrid
       autoHeight
+      checkboxSelection
       density="compact"
       rows={rows}
       columns={columns}
       getRowId={(row) => row.bookId}
       loading={loading}
+      selectionModel={selection}
+      onSelectionModelChange={handleSelection}
     />
   );
 };
--- src/pages/Blank.tsx
+++ src/pages/Blank.tsx
@@ -7,11 +7,16 @@ import * as React from 'react';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import BookForm from '@app/components/bookForm/BookForm';
+import Button from '@mui/material/Button';
+import QRCode from 'react-qr-code';
+import Stack from '@mui/material/Stack';
+import Grid from '@mui/material/Grid';
+import {useRef} from 'react';
+import ReactToPrint from 'react-to-print';
 // import {Grid} from '@mui/material';
 
 function TabPanel(props: any) {
   const {children, value, index, ...other} = props;
-
   return (
     <div
       role="tabpanel"
@@ -29,11 +34,55 @@ function TabPanel(props: any) {
   );
 }
 
-const Blank = () => {
-  const [value, setValue] = React.useState(0);
+const QrElement = ({book}: {book: any}) => {
+  return (
+    <Stack>
+      <QRCode
+        value={`
+            {
+                "identifier": "69694242",
+                "data": ${book.libraryBookNumber},
+            }`}
+        size={128}
+      />
+      <p>{book.bookName}</p>
+    </Stack>
+  );
+};
 
+const QrGrid = React.forwardRef(
+  ({selectedData}: {selectedData: Object[]}, ref) => {
+    const gridCore = selectedData.map((book: any) => {
+      return (
+        <Grid item key={book.libraryBookNumber} xs={2}>
+          <QrElement book={book} />
+        </Grid>
+      );
+    });
+    return (
+      <div style={{display: 'none'}}>
+        <div ref={ref as React.RefObject<HTMLDivElement>}>
+          <Grid container spacing={1}>
+            {gridCore}
+          </Grid>
+        </div>
+      </div>
+    );
+  }
+);
+
+const Blank = () => {
+  const QrRef = useRef(null);
+  const [value, setValue] = React.useState(0);
+  const [selectedData, setSelectedData] = React.useState([]);
+
+  const handlePrintQR = () => {};
   const handleChange = (event: any, newValue: any) => {
     setValue(newValue);
+  };
+  const getSelectionDatafromGrid = (selectionDataFromGrid: any) => {
+    setSelectedData(selectionDataFromGrid);
+    // console.log(selectedData);
   };
   return (
     <>
@@ -46,8 +95,22 @@ const Blank = () => {
         <Tab label="Add a book" />
       </Tabs>
       <TabPanel value={value} index={0}>
-        <div style={{height: 'auto', width: '100%'}}>
-          <BookList />
+        <div
+          style={{
+            height: 'auto',
+            width: '100%'
+          }}
+        >
+          <BookList getSelection={getSelectionDatafromGrid} />
+          <ReactToPrint
+            trigger={() => (
+              <Button variant="contained" onClick={handlePrintQR}>
+                Print QR
+              </Button>
+            )}
+            content={() => QrRef.current}
+          />
+          <QrGrid selectedData={selectedData} ref={QrRef} />
         </div>
       </TabPanel>
       <TabPanel value={value} index={1}>
