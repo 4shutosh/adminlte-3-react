6c777fbe52d08357f2a022b16517d2e401c7b802
Kartik Kshirsagar <kartikpkshirsagar@gmail.com>
2022-05-16 14:32:43 +0530

    minor bug fix

--- src/components/addAnnouncementForm/AddAnnouncementForm.tsx
+++ src/components/addAnnouncementForm/AddAnnouncementForm.tsx
@@ -26,6 +26,7 @@ const AddAnnouncementForm = () => {
         }
         setAnnouncementTitle('');
         setAnnouncementMessage('');
+        setAnnouncementLink('');
       })
       .catch((error) => {
         console.log(error);
